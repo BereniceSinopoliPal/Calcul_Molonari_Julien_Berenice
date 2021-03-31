bb0b546c6381a289a54fe4560d27dd60de0687a2
Julien Horsin <julien.horsin@mines-paristech.fr>
2021-03-31 13:46:20 +0200

    trame principale

---
+++ test.py
@@ -0,0 +1,39 @@
+class Params():
+    def __init__(self, params_range, value):
+        self.range=params_range
+
+    def pertub
+
+
+
+
+
+class Column():
+    def __init__( ### propriété physique comme attribut
+                                            ):
+        self._h = ##taille
+        self._dh = ## pas spatial
+
+        self.distribution = None
+
+    def run_MCMC():
+        self.distribtuio = list
+
+    def run_modele_direct():
+        
+
+
+#### Exemple utilisation
+
+colums = Columns(8m, 0.1m)
+
+k = Params(range, sigma)
+n=..
+lambda_s=...
+sigma_obs=....
+
+colums.run_MCMC(nbsim,k,....)
+
+columns.run_modele_direct()
+
+   
