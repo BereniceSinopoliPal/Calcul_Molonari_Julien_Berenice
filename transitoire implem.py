3e9a97c70d51d83820cccc36759ec318fae322cb
Julien Horsin <julien.horsin@mines-paristech.fr>
2021-04-07 15:58:49 +0200

    fin transitoire

--- transitoire implem.py
+++ transitoire implem.py
@@ -7,15 +7,35 @@ def run_modele_direct():
 p_init = np.linspace(1,2,100).reshape((100,1))
 print(p_init)
 def run_k_solve(K, Ss ,dt, dz, Zi, z0, zn, alpha = 0.7):
-    A=np.zeros((100,100))
-    A[0][0]=1
-    A[99][99]=1
 
-    B=np.zeros((100,100))
-    B[0][0]=0
-    B[99][99]=0
+    n= Zi.shape[0]
     
-    for i in range(1,99):
+    A=np.zeros((n,n))
+    B=np.zeros((n,n))
+    A[0][0]= 1
+    A[1][0]= 2*alpha *K*4/(3*(dz)**2) ##terme limite
+    A[1][1]= -alpha*K*4/(dz)**2
+    A[1][2]= alpha *K*4/(3*(dz)**2)
+
+    A[n-1][n-1]=1
+    A[n-2][n-3]= (alpha)*K*4/(3*(dz)**2)
+    A[n-2][n-2]= -(alpha)*K*4/(dz)**2
+    A[n-2][n-1] = 2*(alpha)*K*4/(3*(dz)**2)
+
+    
+    B[0][0]=0
+    B[1][0]= 2*(1-alpha)*K*4/(3*(dz)**2)
+    B[1][1]= -(1-alpha)*K*4/(dz)**2
+    B[1][2] = (1-alpha)*K*4/(3*(dz)**2)
+
+    B[n-1][n-1]=0
+    B[n-2][n-3]= (1-alpha)*K*4/(3*(dz)**2)
+    B[n-2][n-2]= -(1-alpha)*K*4/(dz)**2
+    B[n-2][n-1] = 2*(1-alpha)*K*4/(3*(dz)**2)
+
+   
+    
+    for i in range(2,n-2):
         A[i][i-1]=K*alpha/dz**2
         A[i][i]=-(2*K*alpha/dz**2) - Ss/dt
         A[i][i+1]=K*alpha/dz**2
@@ -26,7 +46,7 @@ def run_k_solve(K, Ss ,dt, dz, Zi, z0, zn, alpha = 0.7):
 
     C = np.matmul(B,Zi)
     C[0]= z0
-    C[99]=zn
+    C[n-1]=zn
 
     res = np.linalg.solve(A,C)
     return res
