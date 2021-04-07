8d8830c6ee8643e5e24d0595cbc882b0cd0bfee3
Julien Horsin <julien.horsin@mines-paristech.fr>
2021-04-07 16:39:23 +0200

    fin transitoire hydro correct

--- transitoire implem.py
+++ transitoire implem.py
@@ -5,33 +5,34 @@ def run_modele_direct():
     pass
 
 p_init = np.linspace(1,2,100).reshape((100,1))
-print(p_init)
+print(p_init) #réfléchir à passer la porosité plutot de Ss
 def run_k_solve(K, Ss ,dt, dz, Zi, z0, zn, alpha = 0.7):
 
     n= Zi.shape[0]
     
     A=np.zeros((n,n))
     B=np.zeros((n,n))
+
     A[0][0]= 1
-    A[1][0]= 2*alpha *K*4/(3*(dz)**2) ##terme limite
-    A[1][1]= -alpha*K*4/(dz)**2
-    A[1][2]= alpha *K*4/(3*(dz)**2)
+    A[1][0]= 8*alpha*K/(3*((dz)**2)) ##terme limite
+    A[1][1]= -alpha*K*4/((dz)**2) - Ss/dt
+    A[1][2]= 4*alpha*K/(3*((dz)**2))
 
     A[n-1][n-1]=1
     A[n-2][n-3]= (alpha)*K*4/(3*(dz)**2)
-    A[n-2][n-2]= -(alpha)*K*4/(dz)**2
-    A[n-2][n-1] = 2*(alpha)*K*4/(3*(dz)**2)
+    A[n-2][n-2]= -(alpha)*K*4/((dz)**2) - Ss/dt
+    A[n-2][n-1] = 8*(alpha)*K/(3*(dz)**2)
 
     
-    B[0][0]=0
-    B[1][0]= 2*(1-alpha)*K*4/(3*(dz)**2)
-    B[1][1]= -(1-alpha)*K*4/(dz)**2
-    B[1][2] = (1-alpha)*K*4/(3*(dz)**2)
+    B[0][0]=1
+    B[1][0]= -2*(1-alpha)*K*4/(3*(dz)**2)
+    B[1][1]= (1-alpha)*K*4/(dz)**2 - Ss/dt
+    B[1][2] = -(1-alpha)*K*4/(3*(dz)**2)
 
-    B[n-1][n-1]=0
-    B[n-2][n-3]= (1-alpha)*K*4/(3*(dz)**2)
-    B[n-2][n-2]= -(1-alpha)*K*4/(dz)**2
-    B[n-2][n-1] = 2*(1-alpha)*K*4/(3*(dz)**2)
+    B[n-1][n-1]=1
+    B[n-2][n-3]= -(1-alpha)*K*4/(3*(dz)**2)
+    B[n-2][n-2]= (1-alpha)*K*4/(dz)**2 - Ss/dt
+    B[n-2][n-1] = -8*(1-alpha)*K/(3*(dz)**2)
 
    
     
@@ -50,7 +51,6 @@ def run_k_solve(K, Ss ,dt, dz, Zi, z0, zn, alpha = 0.7):
 
     res = np.linalg.solve(A,C)
     return res
-
 
 for i in range(5):
     plt.scatter(p_init,range(len(p_init)),label='%s' %i)
